b41f76bf6d1b7f232707fcd277409887569543de
0008ua <0008.ua@gmail.com>
2022-03-23 18:26:19 +0200

    add analytics auto update

--- scores/src/app/modules/analytics/train/train.page.ts
+++ scores/src/app/modules/analytics/train/train.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { IGamer } from 'src/app/interfaces';
+import { IGame, IGamer, IUser } from 'src/app/interfaces';
 import * as fromAnalyticsActions from 'src/app/store/actions/analytics.actions';
+import { GameService } from 'src/app/store/game-data.service';
 import { selectRaiting, selectLoading } from 'src/app/store/reducers/analytics.reducer';
 
 import { environment } from 'src/environments/environment';
@@ -20,12 +21,23 @@ export class TrainPage implements OnInit {
   stat: any = this.stats[0];
   math = Math;
   array =Array;
-  
+
+  games$: Observable<IGame[]>;
+  gamers$: Observable<IGamer[]>;
+
   constructor(
     private store: Store,
+    private gameService: GameService,
   ) { }
 
   ngOnInit() {
+    this.games$ = this.gameService.entities$;
+    this.games$
+      .subscribe(()=>
+        this.store.dispatch(fromAnalyticsActions[this.stat._id]({ gameType: 'train' }))
+       );
+
+
     this.store.dispatch(fromAnalyticsActions[this.stats[0]._id]({gameType: 'train'}));
     this.analytics$ = this.store.select(selectRaiting);
     this.loading$ = this.store.select(selectLoading);
